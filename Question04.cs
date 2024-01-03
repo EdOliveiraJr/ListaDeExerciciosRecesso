9f94fb53761923b78355ba195d60ba9590973a23
Edvaldo Oliveira <edvaldooliveira@alu.ufc.br>
2024-01-03 17:06:01 -0300

    feat(Question04): implementado a questão04

---
+++ Question04.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Numerics;
+
+namespace ListaDeExerciciosRecesso
+{
+    internal static class Question04
+    {
+        internal static void Show()
+        {
+            Console.WriteLine("Digite o tamanho do Array que deseja ordenar: \n");
+            var tamanho = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = new int[tamanho];
+            
+            for(int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine("Digite um número para incluir no Array: \n");
+                arr[i] = (Convert.ToInt16(Console.ReadLine()));
+            }
+
+           Array.Sort(arr);
+
+            foreach (var item in arr)
+            {
+                Console.Write( item + " ");
+            }
+
+        }
+    }
+}
