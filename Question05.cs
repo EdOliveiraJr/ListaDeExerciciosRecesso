bf4905a21004a06ee3989e4dd2b2a7f5d1127eb7
Edvaldo Oliveira <edvaldooliveira@alu.ufc.br>
2024-01-03 17:20:18 -0300

    feat(Question05): implementação da questão 05

---
+++ Question05.cs
@@ -0,0 +1,38 @@
+﻿
+
+namespace ListaDeExerciciosRecesso
+{
+    internal class Question05
+    {
+        private static bool EhPalindromo(string? str)
+        {
+            str = str.Replace(" ", "").ToLower();
+
+            for (int i = 0, j = str.Length - 1; i < j; i++, j--)
+            {
+                if (str[i] != str[j])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        internal static void Show()
+        {
+            Console.Write("Digite uma palavra: \n");
+            string palavra = Console.ReadLine();
+
+            if (EhPalindromo(palavra))
+            {
+                Console.WriteLine($"{palavra} é um palíndromo.\n");
+            }
+            else
+            {
+                Console.WriteLine($"{palavra} não é um palíndromo.\n");
+            }
+        }
+
+    }
+}
