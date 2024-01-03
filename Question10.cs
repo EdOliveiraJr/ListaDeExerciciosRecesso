a8c3252a6acf54f88dea070fb3e15fb07c0e5d7f
Edvaldo Oliveira <edvaldooliveira@alu.ufc.br>
2024-01-03 20:01:07 -0300

    feat(Question10): implementando a questão 10

---
+++ Question10.cs
@@ -0,0 +1,41 @@
+﻿
+namespace ListaDeExerciciosRecesso
+{
+    internal class Question10
+    {
+        private static Dictionary<string, int> AnalisarFrequenciaPalavras(string texto)
+        {
+            string[] palavras = texto.Split(new[] { ' ', '\t', '\n', '\r', '.', ',', ';', ':', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<string, int> frequenciaPalavras = new Dictionary<string, int>();
+
+            foreach (string palavra in palavras)
+            {
+                string palavraFormatada = palavra.ToLower(); 
+                if (frequenciaPalavras.ContainsKey(palavraFormatada))
+                {
+                    frequenciaPalavras[palavraFormatada]++;
+                }
+                else
+                {
+                    frequenciaPalavras[palavraFormatada] = 1;
+                }
+            }
+
+            return frequenciaPalavras;
+        }
+        internal static void Show()
+        {
+            Console.WriteLine("Digite um texto:");
+            string texto = Console.ReadLine();
+
+            Dictionary<string, int> frequenciaPalavras = AnalisarFrequenciaPalavras(texto);
+
+            Console.WriteLine("\nFrequência de palavras no texto:");
+            foreach (var par in frequenciaPalavras)
+            {
+                Console.WriteLine($"{par.Key}: {par.Value} vezes");
+            }
+        }
+    }
+}
